4af08040e4802457192d2e8711b00cae38d85139
cbcruk <cbcruk@users.noreply.github.com>
2021-04-06 22:06:31 +0900

    :star: feature: scan 프리뷰 기능 추가

--- src/components/Scan.js
+++ src/components/Scan.js
@@ -3,6 +3,7 @@ import { Text, View, StyleSheet } from 'react-native'
 import { BarCodeScanner } from 'expo-barcode-scanner'
 import { Snackbar } from 'react-native-paper'
 import { observer, useLocalObservable } from 'mobx-react-lite'
+import { runInAction } from 'mobx'
 import { RootContext } from './Provider'
 
 const Scan = observer(() => {
@@ -13,9 +14,17 @@ const Scan = observer(() => {
     setScanned(scanned) {
       this.scanned = scanned
     },
-    handleBarCodeScanned(target) {
-      this.scanned = true
-      this.target = target
+    async handleBarCodeScanned(target) {
+      const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=isbn:${target.data}`
+      )
+      const data = await response.json()
+      const [book] = data.items
+
+      runInAction(() => {
+        this.scanned = true
+        this.target = { ...target, book }
+      })
     },
   }))
   const { scanned, setScanned, handleBarCodeScanned, target } = scanObservable
@@ -39,7 +48,9 @@ const Scan = observer(() => {
           },
         }}
       >
-        <Text onPress={() => setScanned(false)}>{target?.data}</Text>
+        <Text onPress={() => setScanned(false)}>
+          {target?.data} - {target?.book?.volumeInfo?.title}
+        </Text>
       </Snackbar>
     </View>
   )
